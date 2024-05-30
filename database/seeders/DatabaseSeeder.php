7f3bbd6e86d273c8aa101488794cea95493acae2
Aliz-011 <aryatangkas56@gmail.com>
2024-05-31 05:26:08 +0900

    implemented "spatie"

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        $this->call([
+            RoleSeeder::class
+        ]);
+        User::factory()->create([
+            'id' => Str::createUuidsNormally(),
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ])->assignRole('superadmin');
+        User::factory()->create([
+            'id' => Str::createUuidsNormally(),
+            'name' => 'Lora Nikijuluw',
+            'email' => 'lora@example.com',
+        ])->assignRole('admin');
+    }
+}
