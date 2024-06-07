3b2853551ea10bc3c27fe099939ba5fe1a16b4a9
Valac12 <71829515+Valac12@users.noreply.github.com>
2024-06-07 20:25:46 +0900

    Update alternatif-form.tsx

--- resources/js/Pages/Alternatif/alternatif-form.tsx
+++ resources/js/Pages/Alternatif/alternatif-form.tsx
@@ -1,9 +1,10 @@
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { router, usePage } from "@inertiajs/react";
+import { usePage } from "@inertiajs/react";
+import { router } from "@inertiajs/react";
 import { toast } from "sonner";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
 import { Input } from "@/Components/ui/input";
 import { Button } from "@/Components/ui/button";
@@ -42,34 +43,6 @@ const AlternatifForm = ({ defaultValues, id }: Props) => {
     const [isLoading, setIsLoading] = useState(false);
     const { flash, errors } = usePage<PageProps>().props;
 
-    const form = useForm<FormValues>({
-        resolver: zodResolver(formSchema),
-        defaultValues: defaultValues || {
-            nama: "",
-        },
-    });
-
-    const handleSubmit = (values: FormValues) => {
-        try {
-            setIsLoading(true);
-            if (!!id) {
-                router.patch(`/alternatifs/${id}`, { ...values }),
-                    {
-                        forceFormData: true,
-                    };
-            } else {
-                router.post("/alternatifs", values),
-                    {
-                        forceFormData: true,
-                    };
-            }
-        } catch (error) {
-            toast.error("Something went wrong");
-        } finally {
-            setIsLoading(false);
-        }
-    };
-
     useEffect(() => {
         if (flash?.success) {
             toast.success(flash.success);
@@ -87,6 +60,42 @@ const AlternatifForm = ({ defaultValues, id }: Props) => {
             }
         }
     }, [errors]);
+
+    const form = useForm<FormValues>({
+        resolver: zodResolver(formSchema),
+        defaultValues: defaultValues || {
+            nama: "",
+        },
+    });
+
+    const handleSubmit = (values: FormValues) => {
+        setIsLoading(true);
+        if (id) {
+            router.patch(`/alternatifs/${id}`, values, {
+                onSuccess: () => {
+                    toast.success("Alternatif updated!");
+                    setIsLoading(false);
+                },
+                onError: () => {
+                    toast.error("Something went wrong");
+                    setIsLoading(false);
+                },
+                forceFormData: true,
+            });
+        } else {
+            router.post("/alternatifs", values, {
+                onSuccess: () => {
+                    toast.success("Alternatif created!");
+                    setIsLoading(false);
+                },
+                onError: () => {
+                    toast.error("Something went wrong");
+                    setIsLoading(false);
+                },
+                forceFormData: true,
+            });
+        }
+    };
 
     return (
         <Form {...form}>
@@ -112,7 +121,7 @@ const AlternatifForm = ({ defaultValues, id }: Props) => {
                     )}
                 />
                 <Button disabled={isLoading}>
-                    {!!id ? "Save changes" : "Create alternatif"}
+                    {id ? "Save changes" : "Create alternatif"}
                 </Button>
             </form>
         </Form>
